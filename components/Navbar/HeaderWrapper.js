8c46ef7dc27cade242f5858c8ff671fda1e51d72
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-06-10 23:14:47 +0200

    Creating HeaderWrapper file

---
+++ components/Navbar/HeaderWrapper.js
@@ -0,0 +1,10 @@
+import React from "react"
+import styled from "styled-components"
+
+export default function HeaderWrapper() {
+  return <Wrapper></Wrapper>
+}
+
+export const Wrapper = styled.div`
+  border: 3px solid red;
+`
