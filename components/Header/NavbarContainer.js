fe8215db4b72d939775a7be63cd00e8b1fffc172
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-06-10 23:37:55 +0200

    Creating NavbarContainer file

---
+++ components/Header/NavbarContainer.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export default function NavbarContainer() {
+  return <Wrapper>c33333333</Wrapper>
+}
+
+export const Wrapper = styled.div`
+  border: 3px solid yellow;
+`
