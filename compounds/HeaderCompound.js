928889d051117ddb0a3942345f3d966de4bfdd06
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-06-10 23:57:38 +0200

    Defining Logo file

--- compounds/HeaderCompound.js
+++ compounds/HeaderCompound.js
@@ -1,11 +1,14 @@
 import HeaderContainer from '../components/Header/HeaderContainer'
 import NavbarContainer from '../components/Header/NavbarContainer'
+import Logo from '../components/Header/Logo'
 
 export default function HeaderCompound() {
   return (
     <>
       <HeaderContainer>
-        <NavbarContainer></NavbarContainer>
+        <NavbarContainer>
+          <Logo />
+        </NavbarContainer>
       </HeaderContainer>
     </>
   )
