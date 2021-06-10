71c6fc3688cea0d99747b07dd4c99a623227249c
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-06-10 23:36:40 +0200

    Adding a Background-Image

--- components/Header/HeaderContainer.js
+++ components/Header/HeaderContainer.js
@@ -1,8 +1,10 @@
+import styled from "styled-components"
 
 export default function HeaderContainer() {
-  return <Wrapper></Wrapper>
+  return <Wrapper>xcsczsdfsgeg</Wrapper>
 }
 
 export const Wrapper = styled.div`
-  
+  background: url(/images/misc/home-bg.jpg) left top / cover no-repeat;
+  height: 400px;
 `
