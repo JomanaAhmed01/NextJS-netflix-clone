55792e466d428aef8aee61c5ce5f04d26ff99789
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-06-10 22:35:25 +0200

    Testing HeaderContainer file

---
+++ components/Navbar/HeaderContainer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function HeaderContainer() {
+  return (
+    <Wrapper></Wrapper>
+  )
+}
+
+export const Wrapper = styled.div`
+  border: 3px solid red;
+`
