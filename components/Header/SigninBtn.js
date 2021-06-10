1bf594ecf815750eca7ac39064f3ed881a5c3be6
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-06-11 00:11:43 +0200

    Adding code to SigninBtn file

---
+++ components/Header/SigninBtn.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export default function SigninBtn({ children, ...restProps }) {
+  return <Wrapper {...restProps}>{children}c33333333</Wrapper>
+}
+
+export const Wrapper = styled.div`
+  border: 3px solid yellow;
+`
