fb666b9c2510b1faec05ec406d1a9d0811cab1ce
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-06-12 02:26:24 +0200

    Creating JumboTextWrapper file

--- compounds/JumboCompound.js
+++ compounds/JumboCompound.js
@@ -1,11 +1,14 @@
 import JumboWrapper from '../components/Jumbo/JumboWrapper'
 import JumboItem from '../components/Jumbo/JumboItem'
+import JumboTextWrapper from '../components/Jumbo/JumboTextWrapper'
 
 export default function JumboCompound() {
   return (
     <>
       <JumboWrapper>
-        <JumboItem></JumboItem>
+        <JumboItem>
+          <JumboTextWrapper></JumboTextWrapper>
+        </JumboItem>
       </JumboWrapper>
     </>
   )
