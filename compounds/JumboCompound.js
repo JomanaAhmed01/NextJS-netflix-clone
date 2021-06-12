dcbf2761be0502722d892d4c6253a72ba941a8ef
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-06-12 02:08:35 +0200

    Creating JumboCompound file

---
+++ compounds/JumboCompound.js
@@ -0,0 +1,6 @@
+
+export default function JumboCompound() {
+  return (
+    <></>
+  )
+}
--- pages/home.js
+++ pages/home.js
@@ -1,9 +1,11 @@
 import HeaderCompound from '../compounds/HeaderCompound'
+import JumboCompound from '../compounds/JumboCompound'
 
 export default function Home() {
   return(
     <>
       <HeaderCompound />
+      <JumboCompound />
     </>
   )
 }
