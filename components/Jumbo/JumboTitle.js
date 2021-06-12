cff0310a957cfafb2e4aa1b0dd56e1d9f8ec68f1
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-06-12 02:30:36 +0200

    Creating JumboTitle file

---
+++ components/Jumbo/JumboTitle.js
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+export default function JumboTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}Enjoy on your TV.</Title>
+}
+
+export const Title = styled.h1`
+  color: #ffffff;
+  font-size: 45px;
+  width: 90%;
+  margin-bottom: 1px;
+
+  @media (max-width: 950px) {
+    width: initial;
+    font-size: 35px;
+  }
+
+  @media (max-width: 550px) {
+    width: initial;
+    font-size: 30px;
+  }
+`
--- compounds/JumboCompound.js
+++ compounds/JumboCompound.js
@@ -1,13 +1,16 @@
 import JumboWrapper from '../components/Jumbo/JumboWrapper'
 import JumboItem from '../components/Jumbo/JumboItem'
 import JumboTextWrapper from '../components/Jumbo/JumboTextWrapper'
+import JumboTitle from '../components/Jumbo/JumboTitle'
 
 export default function JumboCompound() {
   return (
     <>
       <JumboWrapper>
         <JumboItem>
-          <JumboTextWrapper></JumboTextWrapper>
+          <JumboTextWrapper>
+            <JumboTitle />
+          </JumboTextWrapper>
         </JumboItem>
       </JumboWrapper>
     </>
