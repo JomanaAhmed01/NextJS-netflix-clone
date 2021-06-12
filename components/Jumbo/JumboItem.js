2c64e039a32ee82c394e6e798266f367c1b1b6e9
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-06-12 02:31:44 +0200

    Adding CSS to the JumboItem file

--- components/Jumbo/JumboItem.js
+++ components/Jumbo/JumboItem.js
@@ -4,4 +4,16 @@ export default function JumboItem({ children, ...restProps }) {
   return <Wrapper {...restProps}>{children}</Wrapper>
 }
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 30px 100px;
+
+  @media (max-width: 950px) {
+    flex-direction: column;
+  }
+
+  @media (max-width: 600px) {
+    padding: 30px 50px;
+  }
+`
