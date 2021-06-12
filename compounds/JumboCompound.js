faf57628bfa2e062d3b5bdad19cce73d4e72eba5
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-06-12 02:37:21 +0200

    Creating JumboImageWrapper file

--- compounds/JumboCompound.js
+++ compounds/JumboCompound.js
@@ -3,6 +3,7 @@ import JumboItem from '../components/Jumbo/JumboItem'
 import JumboTextWrapper from '../components/Jumbo/JumboTextWrapper'
 import JumboTitle from '../components/Jumbo/JumboTitle'
 import JumboSubtitle from '../components/Jumbo/JumboSubtitle'
+import JumboImageWrapper from '../components/Jumbo/JumboImageWrapper'
 
 export default function JumboCompound() {
   return (
@@ -13,6 +14,8 @@ export default function JumboCompound() {
             <JumboTitle />
             <JumboSubtitle />
           </JumboTextWrapper>
+
+          <JumboImageWrapper></JumboImageWrapper>
         </JumboItem>
       </JumboWrapper>
     </>
