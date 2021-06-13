29166da981225033b200c9368142a748548b7650
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-06-13 02:50:20 +0200

    Creating AccordionItem file

---
+++ components/Accordion/AccordionItem.js
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+export default function AccordionItem({ children, ...restProps }) {
+  return <Wrapper {...restProps}>{children}</Wrapper>
+}
+
+export const Wrapper = styled.div``
--- compounds/AccordionCompound.js
+++ compounds/AccordionCompound.js
@@ -1,11 +1,14 @@
 import AccordionWrapper from '../components/Accordion/AccordionWrapper'
 import AccordionTitle from '../components/Accordion/AccordionTitle'
+import AccordionItem from '../components/Accordion/AccordionItem'
 
 export default function AccordionCompound() {
   return (
     <>
       <AccordionWrapper>
         <AccordionTitle />
+
+        <AccordionItem></AccordionItem>
       </AccordionWrapper>
     </>
   )
