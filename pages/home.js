10e2caf63e4e64bfe999248ceeeb6ce5ffb4d284
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-06-13 02:37:20 +0200

    Creating AccordionCompound file

--- pages/home.js
+++ pages/home.js
@@ -1,11 +1,13 @@
 import HeaderCompound from '../compounds/HeaderCompound'
 import JumboCompound from '../compounds/JumboCompound'
+import AccordionCompound from '../compounds/AccordionCompound'
 
 export default function Home() {
   return(
     <>
       <HeaderCompound />
       <JumboCompound />
+      <AccordionCompound />
     </>
   )
 }
