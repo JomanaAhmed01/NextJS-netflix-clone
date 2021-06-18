a48f8d6e8ba19ea50faa0d1e11adbec0cc444745
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-06-18 22:59:32 +0200

    Creating FooterWrapper file

--- compounds/FooterCompound.js
+++ compounds/FooterCompound.js
@@ -1,6 +1,9 @@
+import FooterWrapper from '../components/Footer/FooterWrapper'
 
 export default function FooterCompound() {
   return (
-    <></>
+    <>
+      <FooterWrapper></FooterWrapper>
+    </>
   )
 }
