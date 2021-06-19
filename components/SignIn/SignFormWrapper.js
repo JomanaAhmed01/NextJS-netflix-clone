7fadf8c3fa3ab43b6dd2811da392356ae2850e7d
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-06-19 22:15:10 +0200

    Adding styles to SignFormWrapper file

--- components/SignIn/SignFormWrapper.js
+++ components/SignIn/SignFormWrapper.js
@@ -4,4 +4,12 @@ export default function SignFormWrapper({ children, ...restProps }) {
   return <Wrapper {...restProps}>{children}</Wrapper>
 }
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-top: 20px;
+
+  @media (max-width: 740px) {
+    display: initial;
+  }
+`
