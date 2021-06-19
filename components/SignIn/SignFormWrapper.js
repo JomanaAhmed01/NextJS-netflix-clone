f64cfa43aab6eeaf2e2b0cc2c771dfdb3fa4ee7c
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-06-19 22:13:20 +0200

    Creating SignFormWrapper file

---
+++ components/SignIn/SignFormWrapper.js
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+export default function SignFormWrapper({ children, ...restProps }) {
+  return <Wrapper {...restProps}>{children}ddddddddddd</Wrapper>
+}
+
+export const Wrapper = styled.div``
--- pages/signin.js
+++ pages/signin.js
@@ -1,13 +1,15 @@
 import HeaderContainer from '../components/Header/HeaderContainer'
 import NavbarContainer from '../components/Header/NavbarContainer'
 import Logo from '../components/Header/Logo'
+import SignFormWrapper from '../components/SignIn/SignFormWrapper'
 
 export default function signin() {
   return (
     <>
       <HeaderContainer>
-          <NavbarContainer />
-          <Logo />
+        <NavbarContainer />
+        <Logo />
+        <SignFormWrapper></SignFormWrapper>
       </HeaderContainer>
     </>
   )
