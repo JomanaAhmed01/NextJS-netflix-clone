217b1a6e93232cd2fa22b2d63317f376000347c2
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-06-19 15:17:55 +0200

    Creating SignFormWrapper file

--- pages/signin.js
+++ pages/signin.js
@@ -1,7 +1,9 @@
+import SignFormWrapper from '../components/SignIn/SignFormWrapper'
+
 export default function SignIn() {
   return (
     <>
-      
+      <SignFormWrapper></SignFormWrapper>
     </>
   )
 }
