77a9a79607e51ec84563d2e55809642d954d3913
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-06-19 22:11:18 +0200

    Adding Navbar & Background-image to signin Page

--- pages/signin.js
+++ pages/signin.js
@@ -1,5 +1,14 @@
+import HeaderContainer from '../components/Header/HeaderContainer'
+import NavbarContainer from '../components/Header/NavbarContainer'
+import Logo from '../components/Header/Logo'
+
 export default function signin() {
   return (
-    <></>
+    <>
+      <HeaderContainer>
+          <NavbarContainer />
+          <Logo />
+      </HeaderContainer>
+    </>
   )
 }
