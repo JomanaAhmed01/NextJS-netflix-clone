7eaa7bf4118e6b891b4b55f1fed2f2e596692662
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-06-19 18:24:37 +0200

    Deleting SignIn Page files

--- components/SignIn/SignFormWrapper.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components"
-
-export default function SignFormWrapper({ children, ...restProps }) {
-  return <Wrapper {...restProps}>{children}</Wrapper>
-}
-
-export const Wrapper = styled.div``
--- pages/signin.js
+++
@@ -1,9 +0,0 @@
-import SignFormWrapper from '../components/SignIn/SignFormWrapper'
-
-export default function SignIn() {
-  return (
-    <>
-      <SignFormWrapper></SignFormWrapper>
-    </>
-  )
-}
