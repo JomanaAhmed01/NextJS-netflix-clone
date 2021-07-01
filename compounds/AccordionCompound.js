a95b3db7bf74036ed26d78e2026de4f02779f20a
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-07-01 16:12:15 +0200

    Creating the Function that allows to the FAQ Section to work correctly

--- compounds/AccordionCompound.js
+++ compounds/AccordionCompound.js
@@ -1,19 +1,155 @@
+import { useState } from "react"
 import AccordionWrapper from "../components/Accordion/AccordionWrapper"
 import AccordionTitle from "../components/Accordion/AccordionTitle"
 import AccordionItem from "../components/Accordion/AccordionItem"
+import AccordionHeader from "../components/Accordion/AccordionHeader"
+import AccordionBody from "../components/Accordion/AccordionBody"
 import FeatureSubtitle from "../components/Feature/FeatureSubtitle"
 import OptFormWrapper from "../components/OptForm/OptFormWrapper"
 import OptFormEmail from "../components/OptForm/OptFormEmail"
 import OptFormBtn from "../components/OptForm/OptFormBtn"
 import Separator from "../components/Jumbo/Separator"
+import CloseSlimImage from '../components/Accordion/CloseSlimImage'
+import AddImage from "../components/Accordion/AddImage"
 
 export default function AccordionCompound() {
+  const [FAQState1, setFAQState1] = useState("close")
+  const [FAQState2, setFAQState2] = useState("close")
+  const [FAQState3, setFAQState3] = useState("close")
+  const [FAQState4, setFAQState4] = useState("close")
+  const [FAQState5, setFAQState5] = useState("close")
+
+  function handleClick1() {
+    if (FAQState1 === "close") {
+      setFAQState1("open")
+    } else if (FAQState1 === "open") {
+      setFAQState1("close")
+    }
+  }
+
+  function handleClick2() {
+    if (FAQState2 === "close") {
+      setFAQState2("open")
+    } else if (FAQState2 === "open") {
+      setFAQState2("close")
+    }
+  }
+
+  function handleClick3() {
+    if (FAQState3 === "close") {
+      setFAQState3("open")
+    } else if (FAQState3 === "open") {
+      setFAQState3("close")
+    }
+  }
+
+  function handleClick4() {
+    if (FAQState4 === "close") {
+      setFAQState4("open")
+    } else if (FAQState4 === "open") {
+      setFAQState4("close")
+    }
+  }
+
+  function handleClick5() {
+    if (FAQState5 === "close") {
+      setFAQState5("open")
+    } else if (FAQState5 === "open") {
+      setFAQState5("close")
+    }
+  }
   return (
     <>
       <AccordionWrapper>
         <AccordionTitle />
 
-        <AccordionItem></AccordionItem>
+        <AccordionItem>
+          <AccordionHeader onClick={handleClick1} className="acc-header">
+            What is Netflix?
+            {FAQState1 === "open" ? <CloseSlimImage /> : <AddImage />}
+          </AccordionHeader>
+
+          {FAQState1 === "open" ? (
+            <AccordionBody>
+              Netflix is a streaming service that offers a wide variety of
+              award-winning TV shows, movies, anime, documentaries, and more on
+              thousands of internet-connected devices.
+              <br /> <br />
+              You can watch as much as you want, whenever you want without a
+              single commercial – all for one low monthly price. There's always
+              something new to discover and new TV shows and movies are added
+              every week!
+            </AccordionBody>
+          ) : null}
+        </AccordionItem>
+
+        <AccordionItem>
+          <AccordionHeader onClick={handleClick2} className="acc-header">
+            How much does Netflix coast?
+            {FAQState2 === "open" ? <CloseSlimImage /> : <AddImage />}
+          </AccordionHeader>
+
+          {FAQState2 === "open" ? (
+            <AccordionBody>
+              Watch Netflix on your smartphone, tablet, smart TV, laptop or
+              streaming device, all for one low fixed monthly fee. Plans start
+              from £5.99 a month. No extra costs or contracts.
+            </AccordionBody>
+          ) : null}
+        </AccordionItem>
+
+        <AccordionItem>
+          <AccordionHeader onClick={handleClick3} className="acc-header">
+            Where can I watch?
+            {FAQState3 === "open" ? <CloseSlimImage /> : <AddImage />}
+          </AccordionHeader>
+
+          {FAQState3 === "open" ? (
+            <AccordionBody>
+              Watch anywhere, anytime, on an unlimited number of devices. Sign
+              in with your Netflix account to watch instantly on the web at
+              netflix.com from your personal computer or on any
+              internet-connected device that offers the Netflix app, including
+              smart TVs, smartphones, tablets, streaming media players and game
+              consoles. <br /> <br />
+              You can also download your favorite shows with the iOS, Android,
+              or Windows 10 app. Use downloads to watch while you're on the go
+              and without an internet connection. Take Netflix with you
+              anywhere.
+            </AccordionBody>
+          ) : null}
+        </AccordionItem>
+
+        <AccordionItem>
+          <AccordionHeader onClick={handleClick4} className="acc-header">
+            How do I cancel?
+            {FAQState4 === "open" ? <CloseSlimImage /> : <AddImage />}
+          </AccordionHeader>
+
+          {FAQState4 === "open" ? (
+            <AccordionBody>
+              Netflix is flexible. There are no pesky contracts and no
+              commitments. You can easily cancel your account online in two
+              clicks. There are no cancellation fees – start or stop your
+              account anytime.
+            </AccordionBody>
+          ) : null}
+        </AccordionItem>
+
+        <AccordionItem>
+          <AccordionHeader onClick={handleClick5} className="acc-header">
+            What can I watch on Netflix?
+            {FAQState5 === "open" ? <CloseSlimImage /> : <AddImage />}
+          </AccordionHeader>
+
+          {FAQState5 === "open" ? (
+            <AccordionBody>
+              Netflix has an extensive library of feature films, documentaries,
+              TV shows, anime, award-winning Netflix originals, and more. Watch
+              as much as you want, anytime you want.
+            </AccordionBody>
+          ) : null}
+        </AccordionItem>
 
         <FeatureSubtitle />
         <OptFormWrapper>
@@ -26,10 +162,6 @@ export default function AccordionCompound() {
     </>
   )
 }
-
-
-
-
 
 /*
 import React from 'react';
